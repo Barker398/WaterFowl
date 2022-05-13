ec6d08a7dcaba6fb47235d4a933f929d9a5efba7
Sam Barker <barkersam29@yahoo.com>
2022-05-13 12:44:42 -0500

    first commit

---
+++ src/components/Waterfowl.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Waterfowl.css"
+
+export const  Waterfowl = () => {
+    <>
+        <h1>Waterfowl Species</h1>
+    </>
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import { Waterfowl } from "./components/Waterfowl";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Waterfowl />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
