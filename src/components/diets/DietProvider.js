b5436630955c6f02b19085f7f689bf706272666d
Sam Barker <barkersam29@yahoo.com>
2022-07-01 20:24:49 -0500

    working on provider

---
+++ src/components/diets/DietProvider.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+
+export const DietContext = createContext()
+
+export const DietProvider = (props) => {
+    const [diets, setDiets] = useState([])
+
+    const getDiets = () => {
+        return fetch("http://localhost:8088/diets")
+        .then(res => res.json())
+        .then(setDiets)
+    }
+
+    return (
+        <DietContext.Provider value={{
+            diets, getDiets
+        }}>
+            {props.children}
+        </DietContext.Provider>
+    )
+}
